aa1886a55825dcca91cda046901640ba5e6fb5f4
David_ss <87577349+titanrex@users.noreply.github.com>
2023-08-22 12:29:30 -0400

    Create variables.tf

---
+++ modules/env_secret/variables.tf
@@ -0,0 +1,26 @@
+# variables.tf
+
+variable "project" {
+  type        = string
+  description = ""
+}
+
+variable "secret_id" {
+  type        = string
+  description = ""
+}
+
+variable "secret_data" {
+  description = "Secret data"
+
+}
+
+variable "role" {
+  type        = string
+  description = ""
+}
+
+variable "member" {
+  type        = string
+  description = ""
+}
